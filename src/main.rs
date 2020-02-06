1ae820017ae9757eb749309db095ccd392a5cffd
Roland Szabo <rolisz@gmail.com>
2020-02-06 23:09:29 +0200

    Full working crawler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![feature(option_result_contains)]
 extern crate serde;
 extern crate quick_xml;
 
@@ -66,43 +67,49 @@ fn check_if_exists(page_data: &SiteMap) -> bool {
 
 fn write_file(path: String, content: &String) {
     let dir = fs::create_dir_all(format!("static{}", path)).unwrap();
+    let content = content.replace("https://ghost.rolisz.ro", "https://rolisz.ro");
     fs::write(format!("static{}/index.html", path), content);
 }
 
-fn write_xml(path: String, content: String) {
+fn write_xml(path: String, content: &String) {
     let parent = Path::new(&path).parent().and_then(Path::to_str).unwrap();
+    println!("Parent {} {}", path, parent);
     let dir = fs::create_dir_all(format!("static{}", parent)).unwrap();
-    fs::write(format!("static{}", path), content);
+    let content = content.replace("https://ghost.rolisz.ro", "https://rolisz.ro");
+    fs::write(format!("static{}", path.trim_end_matches("/")), content);
+}
+
+fn has_bad_extension(url: &&str) -> bool{
+    let ext = Path::new(&url).extension();
+    ext.is_none()
+    //return url.ends_with("ico") || url.to_lowercase().ends_with("jpg") || url.to_lowercase().ends_with("png")
 }
 
 fn get_links_from_html(html: &String) -> HashSet<String> {
      Document::from(html.as_str())
             .find(Name("a").or(Name("link")))
+            .filter(|x| !x.attr("rel").contains(&"amphtml".to_string()))
             .filter_map(|n| n.attr("href"))
+            .filter(has_bad_extension)
             .filter_map(|x| {
                 let new_url = Url::parse(x);
-                match new_url {
+                return match new_url {
                     Ok(new_url) => {
                         if new_url.has_host() && new_url.host_str().unwrap() == "ghost.rolisz.ro" {
-                            if !x.ends_with("ico") {
-                                return Some(x.to_string());
-                            }
-                            return None;
+                            return Some(x.to_string())
                         } else {
                             //println!("Rejecting {}", x);
-                            return None;
+                            None
                         }
                     },
                     Err(e) => {
                         // Relative urls are not parsed by Reqwest
-                        if x.starts_with('/') && !x.ends_with("ico") {
-
-                            return Some(format!("https://ghost.rolisz.ro{}", x));
+                        if x.starts_with('/') {
+                            Some(format!("https://ghost.rolisz.ro{}", x))
                         } else {
-                            println!("Parse error {}", x);
-                            return None;
+                            // println!("Parse error {}", x);
+                            None
                         }
-
                     }
                 }
             }).collect::<HashSet<String>>()
@@ -112,6 +119,9 @@ fn get_page(client: &reqwest::blocking::Client, post_url: &String) -> Option<Str
     let page = client.get(post_url).send();
     if let Ok(mut pg) = page {
         println!("Status for {}: {}", post_url, pg.status());
+        if !pg.status().is_success() {
+            return None;
+        }
         let mut buffer = String::new();
         pg.read_to_string(&mut buffer).unwrap();
 
@@ -125,6 +135,17 @@ fn get_page(client: &reqwest::blocking::Client, post_url: &String) -> Option<Str
     return None;
 }
 
+fn get_non_html(client: &reqwest::blocking::Client, url: &String) -> Option<String> {
+    let mut res = client.get(url).send().unwrap();
+    println!("Status: {} {}", res.status(), url);
+
+    let mut body  = String::new();
+    res.read_to_string(&mut body).unwrap();
+    println!("URL to write {}", res.url().path());
+    write_xml(res.url().path().to_string(), &body);
+    return Some(body);
+}
+
 fn main() -> Result<(), reqwest::Error> {
     let mut visited = Arc::new(Mutex::new(HashSet::new()));
     let mut new_links = Arc::new(Mutex::new(HashSet::new()));
@@ -133,41 +154,33 @@ fn main() -> Result<(), reqwest::Error> {
     let client = reqwest::blocking::Client::new();
 
     let starting_url = "https://ghost.rolisz.ro/sitemap.xml".to_string();
-    let mut res = client.get(&starting_url).send()?;
+    let mut body = get_non_html(&client, &starting_url).unwrap();
     visited.lock().unwrap().insert(starting_url);
-    println!("Status: {}", res.status());
-
-    let mut body  = String::new();
-    res.read_to_string(&mut body).unwrap();
-    println!("Body:\n\n{}", body);
     let html: SiteMapIndex = from_str(&body).unwrap();
-    println!("Parsed {:?}", html);
-    write_xml(res.url().path().to_string(), body);
+
+    get_non_html(&client, &"https://ghost.rolisz.ro/sitemap.xsl".to_string());
+    get_non_html(&client, &"https://ghost.rolisz.ro/robots.txt".to_string());
+    get_non_html(&client, &"https://ghost.rolisz.ro/404.html".to_string());
+
+
     for link in html.sitemap {
         let link_url = &link.loc;
-        let mut page = client.get(link_url).send()?;
+
+        let mut body  = get_non_html(&client, link_url).unwrap();
         visited.lock().unwrap().insert(link_url.to_string());
 
-        println!("Status: {}", page.status());
-        let mut body  = String::new();
-        page.read_to_string(&mut body).unwrap();
-
-        println!("Body:{} \n\n{}", link_url, body);
         let sitemap: UrlSet = from_str(&body).unwrap();
-        println!("Links: {:?}", sitemap.url);
-        write_xml(page.url().path().to_string(), body);
-        let pages : Vec<Option<String>> = sitemap.url.par_iter().map(|post_link| {
+
+        sitemap.url.par_iter().for_each(|post_link| {
             if post_link.loc != "https://ghost.rolisz.ro/" {
                 visited.lock().unwrap().insert(post_link.loc.to_string());
             }
             if check_if_exists(post_link) {
-                return None;
+                return
             }
             let res = get_page(&client, &post_link.loc);
-
-            return res;
-
-        }).collect();
+            new_links.lock().unwrap().extend(get_links_from_html(&res.unwrap()));
+        });
 
     }
 
@@ -179,17 +192,15 @@ fn main() -> Result<(), reqwest::Error> {
 
     let mut temp = HashSet::new();
     while !links.is_empty() {
-        for link in links.iter() {
+        links.iter().for_each(|link| {
             if !visited.lock().unwrap().contains(link) {
-                println!("Hello {}", link);
                 let res = get_page(&client, &link);
                 visited.lock().unwrap().insert(link.to_string());
                 if res.is_some() {
-                    temp = get_links_from_html(&res.unwrap());
-                    println!("Why {:#?}", temp)
+                    temp.extend(get_links_from_html(&res.unwrap()));
                 }
             }
-        }
+        });
         links = Vec::new();
         for v in temp.iter() {
             let stuff = visited.lock().unwrap();
